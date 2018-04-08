1e5c65de7d2df398b3922fdf8d5144f9d88917f5
Paul T <ptsouchlos@bellsouth.net>
2018-04-08 17:14:09 -0400

    Fix for GoogleTest setup on Windows
    Resolves #1.

--- cmake/ExternalGoogleTest.cmake
+++ cmake/ExternalGoogleTest.cmake
@@ -8,13 +8,25 @@ if(NOT Git_FOUND)
     message(FATAL_ERROR "Git is required!")
 endif(NOT Git_FOUND)
 
+# ensure to build shared libs on all platforms
+# except for windows. Not really needed for this project.
+if(WIN32)
+    set(BUILD_SHARED_LIBS OFF)
+else()
+    set(BUILD_SHARED_LIBS ON)
+endif(WIN32)
+
+list(APPEND GTEST_CMAKE_ARGS 
+    "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}"
+    "-Dgtest_force_shared_crt=ON"
+    "-DBUILD_GTEST=ON"
+    "-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
+    "-DCMAKE_DEBUG_POSTFIX=")
+
 ExternalProject_Add(GoogleTestExternal
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG "718fd88d8f145c63b8cc134cf8fed92743cc112f"
-    CMAKE_ARGS -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-               -Dgtest_force_shared_crt=ON
-               -DBUILD_GTEST=ON
-               -DBUILD_SHARED_LIBS=ON
+    CMAKE_ARGS ${GTEST_CMAKE_ARGS}
     PREFIX "${EXTERNAL_BUILD_DIR}"
 	# Disable install step
     INSTALL_COMMAND ""
